e74e2a119a245053bf397289c40d06983f9a6c01
DSuhoi <dsuh0i.h8@gmail.com>
2020-08-04 13:09:06 +0300

    Add a printing function in the Array library (C++).

--- Array/arrayAlgos.hpp
+++ Array/arrayAlgos.hpp
@@ -15,14 +15,11 @@
 #include <iostream>
 #include <ctime>
 
+
 //*********************************
 // Основные константы
 //*********************************
 #define DELIM_CHR ' '			// символ разделитель
-#define CARR_CHR '\n'			// символ перевода каретки
-#define FORMAT_COMMAND "%4d"		// команда форматирования
-#define FORMAT_COMMAND_CNT "%4d"	// команда форматирования для доп. данных
-
 
 
 //********************************************
@@ -32,7 +29,6 @@
 // Для заполнения массивов случайными числами раскомментируйте флаг
 // и укажите диапазон для чисел RANDOM_RANGE
 #define RANDOM_RANGE 100
-
 
 
 //*****************************************
@@ -81,17 +77,17 @@ TBase** Arr2Create(const TCount _row, const TCount _column)
 	if(((_row * _column) < 1) || (MAX_ARR_SIZE < (_row * _column)) ){
 		return nullptr;
 	}
-	
+
 	// Выделение памяти под указатели на указатели на элементы
 	TBase** arr = new TBase* [_row];
 	// Установка первого указателя
 	arr[0] = new TBase [_row * _column];
     
-    for(size_t row = 0; row < _row; row++){
+	for(size_t row = 0; row < _row; row++){
 		// Установка следующих указателей
-    	arr[row] = arr[0] + _column*row;
+		arr[row] = arr[0] + _column*row;
     	
-    #ifdef RANDOM_RANGE
+	#ifdef RANDOM_RANGE
 		// Заполнение случайными числами
 		srand(time(NULL));
 		for(size_t column = 0; column < _column; column++){
@@ -101,12 +97,31 @@ TBase** Arr2Create(const TCount _row, const TCount _column)
 		// Обнуление элементов массива
 		memset(arr[row], 0, _column);
 	#endif
-    }
-    
-    return arr;
+	}
+	
+	return arr;
 }
 
 
+// Вывод _len элементов массива Arr
+template <class TBase, class TCount>
+void ArrPrint(TBase* arr, const TCount _len)
+{
+	for(size_t i = 0; i < _len; i++){
+		std::cout << arr[i] << DELIM_CHR;	// Вывод элемента и символа разделителя
+	}
+	std::cout << std::endl;	// Вывод символа перевода каретки
+}
 
-
+// Вывод _row строк и _column столбцов элементов массива Arr
+template <class TBase, class TCount>
+void Arr2Print(TBase** arr, const TCount _row, const TCount _column)
+{
+	for(size_t row = 0; row < _row; row++){
+		for(size_t column = 0; column < _column; column++){
+			std::cout << arr[row][column] << DELIM_CHR;	// Вывод элемента и символа разделителя
+		}
+		std::cout << std::endl;	// Вывод символа перевода каретки
+	}
+}
 #endif
