31471088cf8f44bfcef590c2d57630077a9c5151
DSuhoi <dsuh0i.h8@gmail.com>
2020-08-24 15:04:35 +0300

    Add the Stack class.

---
+++ Stack/stack_algos.hpp
@@ -0,0 +1,17 @@
+/*
+ * stack_algos.hpp
+ * 
+ * Copyright 2020 DSuhoi
+ * Библиотека для работы со стеком
+ * (C++ library)
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
